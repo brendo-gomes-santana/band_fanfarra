5605a651c312157e74bcb93abed4b4cb188127af
Brendo Gomes Santana <brendogs2021@gmail.com>
2024-02-23 10:05:13 -0300

    criando rotas e comecando a desenvolver a page home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <main>
+            <p>Fazendo</p>
+            <h1>Fazendo</h1>
+        </main>
+    )
+}
---
+++ src/router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home";
+
+export default function Rotas(){
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={ <Home/> }/>
+            </Routes>
+        </Router>
+    )
+}
