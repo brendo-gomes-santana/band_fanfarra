1afff7ad97599d7c70df5306f3e5cc72cf522919
Brendo Gomes Santana <brendogs2021@gmail.com>
2024-02-28 12:33:19 -0300

    começando a a criar a tela de painel

--- src/Pages/Painel/index.jsx
+++ src/Pages/Painel/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../../Components/Header';
+import { Main } from './styled';
 
 export default function Painel() {
   return (
-    <div>Painel</div>
+    <>
+      <Header/>
+      <Main>
+        <h1>Teste</h1>
+      </Main>
+    </>
   )
 }
---
+++ src/Pages/Painel/styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Main = styled.section`
+    padding: 7em 1em 0 1em;
+`;
